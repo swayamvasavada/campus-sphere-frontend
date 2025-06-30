c1dd9ad270e5797c43ebb55369e92c799d2c65a4
Swayam Vasavada <swayam.vasavada1505@gmail.com>
2025-06-30 23:58:53 +0530

    Testing live website

--- src/middleware/Authentication.jsx
+++ src/middleware/Authentication.jsx
@@ -3,7 +3,10 @@ import Cookies from "js-cookie";
 
 export default function Auth({ children }) {
     const cookies = Cookies.get("authToken");
+    console.log("Cookies: ", cookies);
+    
     if (!cookies) {
+        console.log("Returning to Login page");
         return <Navigate to="/login" replace />
     }
 
