e29c3563a5b6e6d277cacab497bfdce9bdbe8635
Swayam Vasavada <swayam.vasavada1505@gmail.com>
2025-06-30 20:21:03 +0530

    Added notice module

---
+++ src/Pages/Notice/NoticeDetail.jsx
@@ -0,0 +1,87 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Alert } from '@mui/material';
+
+import "../../assets/styles/base.css";
+import "../../assets/styles/forms.css";
+import "../../assets/styles/view.css";
+
+export default function NoticeDetail() {
+    const { noticeId } = useParams();
+    const [hasError, setError] = useState(false);
+    const [message, setMessage] = useState('');
+
+    const [noticeData, setNoticeData] = useState({});
+    const [userData, setUserData] = useState({});
+
+    useEffect(function () {
+        async function getNoticeData() {
+            if (!noticeId) return;
+
+            setError(false);
+
+            try {
+                const res = await fetch(`${process.env.REACT_APP_API_URL}/notice/view/${noticeId}`, {
+                    method: 'GET',
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+
+                const result = await res.json();
+
+                if (result.hasError) {
+                    setMessage(result.message);
+                    setError(true);
+                    return;
+                }
+
+                console.log("Data: ", result);
+                setNoticeData(result.noticeData);
+                setUserData(result.userData);
+            } catch (error) {
+                console.log("Error: ", error);
+                setMessage("Something went wrong!");
+                setError(true);
+            }
+        }
+
+        getNoticeData();
+    }, [noticeId]);
+
+    return (
+        <main className='card-container'>
+            {hasError && <Alert severity='error'>{message}</Alert>}
+            <h1>Notice</h1>
+            <div className="card">
+                <div className="info-container">
+                    <h3 className='info-title'>Notice Title: </h3>
+                    <span className='info'>{`${noticeData.noticeTitle}`}</span>
+                </div>
+
+                <div className="info-container">
+                    <h3 className="info-title">Issued by: </h3>
+                    <span className='info'><a href={`mailto:${userData?.email}`}> {`${userData?.name?.firstName} ${userData?.middleName} ${userData?.name?.lastName}`} </a></span>
+                </div>
+
+                <div className="info-container">
+                    <h3 className="info-title">Issued on: </h3>
+                    <span className="info">
+                        <i className="fa-solid fa-calendar"></i>
+                        {new Date(noticeData?.issuedOn).toLocaleDateString("en-US", {
+                            year: "numeric",
+                            month: "long",
+                            day: "2-digit"
+                        })}
+                    </span>
+                </div>
+
+                <div className="info-container">
+                    <h3 className='info-title'>Notice message: </h3>
+                    <span className='info'>{noticeData?.noticeMessage}</span>
+                </div>
+            </div>
+        </main >
+    );
+}
---
+++ src/Pages/Notice/NoticeView.jsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, Alert } from '@mui/material';
+
+import "../../assets/styles/base.css";
+import "../../assets/styles/forms.css";
+import "../../assets/styles/view.css";
+
+export default function ViewNotice() {
+    const [noticeList, setList] = useState([]);
+    const [hasError, setError] = useState(null);
+    const [message, setMessage] = useState('');
+
+    useEffect(function () {
+        async function fetchNoticeList() {
+            try {
+                setError(false);
+
+                const res = await fetch(`${process.env.REACT_APP_API_URL}/notice/`, {
+                    method: 'GET',
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+                });
+
+                const result = await res.json();
+
+                if (result.hasError) {
+                    setError(true);
+                    setMessage(result.message);
+                    return;
+                }
+
+                setList(result.data);
+            } catch (error) {
+                setError(true);
+                setMessage("Something went wrong!");
+            }
+        }
+
+        fetchNoticeList();
+    }, []);
+
+    return (
+        <main className="card">
+            <h1>Book view</h1>
+            {hasError ? <Alert severity='error'> {message} </Alert> : ""}
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell> <Typography variant="h6" fontWeight="bold">  Notice Title  </Typography></TableCell>
+                            {/* <TableCell> <Typography variant="h6" fontWeight="bold">  Issued by  </Typography></TableCell> */}
+                            <TableCell align="right"><Typography variant="h6" fontWeight="bold"> Issued on </Typography></TableCell>
+                            <TableCell align="right"> <Typography variant="h6" fontWeight="bold"> View </Typography></TableCell>
+                        </TableRow>
+                    </TableHead>
+
+                    {
+                        noticeList.length === 0 &&
+                        <>
+                            <i className="fa-solid fa-ban fa-2xl"></i>
+                            <p>Seems like there is no content to display</p>
+                        </>
+                    }
+
+                    <TableBody>
+                        {noticeList.map((notice) => (
+                            <TableRow
+                                key={notice._id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row"> {notice.noticeTitle} </TableCell>
+                                {/* <TableCell component="th" scope="row"> {notice.author} </TableCell> */}
+                                <TableCell align="right">
+                                    <i className="fa-solid fa-calendar" style={{backgroundColor: "#000000"}}></i>
+                                    {new Date(notice.issuedOn).toLocaleDateString("en-US", {
+                                        year: 'numeric',
+                                        month: 'long',
+                                        day: '2-digit',
+                                        weekday: 'short',
+                                    })}
+                                </TableCell>
+                                <TableCell align="right"> <a href={`/notice/view/${notice._id}`}> <i className="fa-solid fa-eye info-btn" title='View Book'></i> </a> </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </main>
+    )
+}
